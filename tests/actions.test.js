6f5987504f0fec8e32977d2e9e4efae5e92e45f5
Gemma Black <gblack@googlemail.com>
2018-04-13 18:17:50 +0100

    GetPreviousSlide separated out into own test

--- tests/actions.test.js
+++ tests/actions.test.js
@@ -8,29 +8,6 @@ import GetCurrentSlide from '../src/ActionHelper/GetCurrentSlide.js';
 
 describe('Slide Instance:', function() {
 
-    
-    describe('GetPreviousSlide', function() {
-        it('should fail if slide is not provided', function() {
-            assert.throws(function() {
-                let slider = actions.Init(2);
-                GetPreviousSlide();
-            }, /Getting previous slide requires state/);
-        });
-
-        it('should get next decremental slide if not on first slide', function() {
-            let slider = actions.Init(2);
-            slider = actions.TransitionTo(slider, 2);
-            slider = actions.TransitionTo(slider, GetPreviousSlide(slider));
-            assert.equal(1, slider.transitionTo);
-        });
-
-        it('should get last slide if on first slide', function() {
-            let slider = actions.Init(2);
-            slider = actions.TransitionTo(slider, GetPreviousSlide(slider));
-            assert.equal(2, slider.transitionTo);
-        });
-    });
-
     describe('TransitionToNextSlide', function() {
         it('should go to next incremental slide 1 -> 2', function() {
             let slider = actions.Init(2);
